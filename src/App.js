7f064f4413bf0397ec0431c7b0ce2a3337bc0b15
elio.lima <elio.lima@opah.com.br>
2022-11-01 23:21:12 -0300

    criei componente formulario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from './componentes/banner/index.js';
-import CampoTexto from './componentes/campoTexto';
+import Formulario from './componentes/formulario/index.js';
 
 function App() {
   return (
     <div className='App'>
       <Banner />
-      <CampoTexto label='Nome' placeholder='Digite seu nome' />
-      <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
-      <CampoTexto label='Imagem' placeholder='Digite o endere seu nome' />
+      <Formulario />
     </div>
   );
 }
