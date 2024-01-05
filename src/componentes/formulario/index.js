5b015bcc1476cb9cb2a7730b5e4d332145aa70dd
Abigail Lima <abigaillima@MacBook-Pro-de-Abigail.local>
2024-01-05 17:08:39 -0300

    Criei componente Lista Suspensa e Botao

--- src/componentes/formulario/index.js
+++ src/componentes/formulario/index.js
@@ -1,14 +1,28 @@
-import CampoTexto from '../campoTexto';
-import './Formulario.css';
+import Botao from "../botao";
+import CampoTexto from "../campoTexto";
+import ListaSuspensa from "../listaSuspensa";
+import "./Formulario.css";
 
 const Formulario = () => {
+  const times = [
+    "Programação",
+    "Front-End",
+    "Data Science",
+    "Devops",
+    "Ux e Design",
+    "Mobile",
+    "Inivação e Gestão",
+  ];
+
   return (
-    <section className='formulario'>
+    <section className="formulario">
       <form>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto label='Nome' placeholder='Digite seu nome' />
-        <CampoTexto label='Cargo' placeholder='Digite seu cargo' />
-        <CampoTexto label='Imagem' placeholder='Digite o endere seu nome' />
+        <CampoTexto label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto label="Imagem" placeholder="Digite o endere seu nome" />
+        <ListaSuspensa label="Time" itens={times} />
+        <Botao>Criar card</Botao>
       </form>
     </section>
   );
