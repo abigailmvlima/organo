e5b692edf855ad8164e1c4ca1ff724419fd62326
abigail.lima <abigaillima@MacBook-Pro-de-Abigail.local>
2024-01-06 17:57:06 -0300

    complemento botao

--- src/componentes/campoTexto/index.js
+++ src/componentes/campoTexto/index.js
@@ -1,11 +1,27 @@
-import './CampoTexto.css';
+import { useState } from "react";
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
   const placeholderModificado = `${props.placeholder}...`;
+
+  // let valor = "Abigail Lima";
+
+  const [valor, setValor] = useState;
+
+  const aoDigitado = (evento) => {
+    valor = evento.target.value;
+    console.log(valor);
+  };
+
   return (
-    <div className='campo-texto'>
+    <div className="campo-texto">
       <label>{props.label}</label>
-      <input placeholder={placeholderModificado} />
+      <input
+        value={valor}
+        onChange={aoDigitado}
+        required={props.obrigatorio}
+        placeholder={placeholderModificado}
+      />
     </div>
   );
 };
--- src/componentes/listaSuspensa/index.js
+++ src/componentes/listaSuspensa/index.js
@@ -2,9 +2,9 @@ import "./ListaSuspensa.css";
 
 const ListaSuspensa = (props) => {
   return (
-    <div>
+    <div className="lista-suspensa">
       <label>{props.label}</label>
-      <select>
+      <select required={props.required}>
         {props.itens.map((item) => {
           return <option key={item}>{item}</option>;
         })}
