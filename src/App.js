4914fb74a4c4f4f6d015068d9da142d3f72b0d7e
abigail.lima <abigaillima@MacBook-Pro-de-Abigail.local>
2024-01-18 19:48:36 -0300

    criei componente time

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Banner from "./componentes/banner";
 import Formulario from "./componentes/formulario";
+import Time from "./componentes/time";
 
 function App() {
   const [colaboradores, setColaboradores] = useState([]);
@@ -18,6 +19,9 @@ function App() {
           aoNovoColaboradorAdicionado(colaborador)
         }
       />
+      <Time nome="Programação" />
+      <Time nome="Front-End" />
+      <Time nome="Date Science" />
     </div>
   );
 }
