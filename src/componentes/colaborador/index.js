60985bda495b7a9f469e488f52587b452bb4dcaf
abigail.lima <abigaillima@MacBook-Pro-de-Abigail.local>
2024-01-18 20:57:28 -0300

    criei componente colaborador

---
+++ src/componentes/colaborador/index.js
@@ -0,0 +1,17 @@
+import "./Colaborador.css";
+
+const Colaborador = () => {
+  return (
+    <div className="colaborador">
+      <div className="cabecalho">
+        <img src="http://github.com/abigailmvlima.png" alt="Abigail Lima" />
+      </div>
+      <div className="rodape">
+        <h4>Abigail Lima</h4>
+        <h5>Desenvolvedora</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
